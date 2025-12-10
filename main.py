1b513165b737c04482eb42166d466e2b97bd21fd
farulivan <farul@nabitu.id>
2025-12-10 20:53:53 +0700

    feat: add cli arg to run specific book

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
+
 from stats import count_words, get_book_text, count_characters, sort_characters
 
 def main():
-  book_path = "books/frankenstein.txt"
+  if len(sys.argv) != 2:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
+  book_path = sys.argv[1]
   book_text = get_book_text(book_path)
   num_words = count_words(book_text)
   characters_dict = count_characters(book_text)
